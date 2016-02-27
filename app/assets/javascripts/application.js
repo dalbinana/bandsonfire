b25469213e02946d12ae69b15fde69f8a759b1a4
daniel_albinyana <dani@commonpeoplei.com>
2016-02-27 10:02:02 +0100

    starting JS object oriented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
+//= require bootstrap.js
 
+if (typeof window.KeepsLiftingMe === 'undefined') {
+  window.KeepsLiftingMe = {};
+}
+KeepsLiftingMe.init = function () {
+  var spoti = new KeepsLiftingMe.Spotify;
+ 
+
+  // $('form').on('submit', spoti.fetchSong.bind(spoti));
+}
+$(document).on('ready', KeepsLiftingMe.init);
 
 
 
--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -2,9 +2,24 @@ class BandsController < ApplicationController
 	def index
 		@user = current_user.id 
 		@bands = Band.where user_id: @user
+		@band = Band.new 
 
 		
 	end	
+	def create
+		@user = current_user.id
+		@band = Band.new band_params
+	if @band.save
+      flash[:notice] = "Entry created successfulluy"
+      redirect_to action: 'index', controller:'bands'
+    else
+      render 'index'
+     end
+    end	
+
+
+	
+
 
 
 
